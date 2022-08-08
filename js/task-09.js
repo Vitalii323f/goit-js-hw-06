6fab87b392286676ae01dda9c866a1a3509ae6e8
Vitalii Shtanko <patas323f@gmail.com>
2022-08-08 12:36:04 +0300

    Закончил 9 задание

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
+
+const bodyBgColorEl = document.querySelector('body')
+const spanEl = document.querySelector('.color')
+const btnChangeColorEl = document.querySelector('.change-color')
+
+btnChangeColorEl.addEventListener('click', changeColor)
+
+function changeColor(event) {
+  const functionHexColor = getRandomHexColor(event)
+
+  bodyBgColorEl.style.backgroundColor = functionHexColor
+
+  spanEl.textContent = functionHexColor
+}
+//  console.log(spanRef);
